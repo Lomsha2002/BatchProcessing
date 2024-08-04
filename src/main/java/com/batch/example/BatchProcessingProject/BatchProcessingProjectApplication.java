361d1f5fc79d3b287c04a474f30661058fe074d9
lomsha.agrawal@arthmate.co.in <lomsha.agrawal@arthmate.co.in>
2024-08-04 17:47:41 +0530

    initial commit

---
+++ src/main/java/com/batch/example/BatchProcessingProject/BatchProcessingProjectApplication.java
@@ -0,0 +1,14 @@
+package com.batch.example.BatchProcessingProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import javax.swing.*;
+
+@SpringBootApplication
+public class BatchProcessingProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BatchProcessingProjectApplication.class, args);
+	}
+
+}
